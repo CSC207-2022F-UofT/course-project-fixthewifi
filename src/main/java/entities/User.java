7f2e82e854ac07c2ebbc49e1ec74a7752b2eb8ed
xingjianll <4396kevinliu@gmail.com>
2022-11-09 20:12:46 -0500

    Renamed package names to lowercase.

---
+++ src/main/java/entities/User.java
@@ -0,0 +1,17 @@
+package entities;
+
+/** Represents a user.
+ * @author Kevin Liu
+ * @version 1.0
+ * @since 1.0
+ */
+public class User
+{
+    int id;
+    UserProfile userprofile;
+    String status;
+    List <Chat> chats
+    FriendsList friendsList
+
++ addChat(Chat chat)
+}
