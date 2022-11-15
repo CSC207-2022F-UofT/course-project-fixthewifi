9bf754bd9e59d4df5626b45f0c20fa3fb27f6726
Joe_Admin <zhuocd.zhang@mail.utoronto.ca>
2022-11-14 21:51:25 -0500

    delete friend feature finished add an interface adapter for output

--- src/main/java/usecases/friendinteractors/addfriend/add_friend_output_model.java
+++
@@ -1,9 +0,0 @@
-package usecases.friendinteractors.addfriend;
-
-public class add_friend_output_model {
-    int requesterid;
-
-    public add_friend_output_model(int requesterid){
-        this.requesterid = requesterid;
-    }
-}
--- src/main/java/usecases/friendinteractors/deletefriend/delete_friend_output_model.java
+++
@@ -1,4 +0,0 @@
-package usecases.friendinteractors.deletefriend;
-
-public class delete_friend_output_model {
-}
