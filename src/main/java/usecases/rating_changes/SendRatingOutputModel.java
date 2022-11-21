796c4c22b675678e39b115ae95c342ccf6b3fc76
David Lim <73053181+davidlyj@users.noreply.github.com>
2022-11-20 22:27:46 -0500

    Created ChangeRatingOutputAdapter.java
    For clean architecture. Change was as per requested by Kotya

--- src/main/java/usecases/rating_changes/SendRatingOutputModel.java
+++ src/main/java/usecases/rating_changes/SendRatingOutputModel.java
@@ -4,6 +4,14 @@ package usecases.rating_changes;
 public class SendRatingOutputModel {
     int receiverUid;
     double avgRating;
+    
+    public int getReceiverUid() {
+        return receiverUid;
+    }
+
+    public double getAvgRating() {
+        return avgRating;
+    }
 
     public SendRatingOutputModel(int receiverUid, double avgRating) {
         this.receiverUid = receiverUid;
