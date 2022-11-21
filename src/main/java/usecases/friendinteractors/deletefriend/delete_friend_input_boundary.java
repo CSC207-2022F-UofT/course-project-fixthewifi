fa151c03f89cbb84785b9a27e9edff177b6fa286
Joe Z. Zhang <zhuocd.zhang@mail.utoronto.ca>
2022-11-20 20:28:17 -0500

    tests are useless because DS is not complete tests are for DS not for Interactor if we really write them

--- src/main/java/usecases/friendinteractors/deletefriend/delete_friend_input_boundary.java
+++ src/main/java/usecases/friendinteractors/deletefriend/delete_friend_input_boundary.java
@@ -4,5 +4,5 @@ import java.util.ArrayList;
 
 public interface delete_friend_input_boundary {
     public void DeleteFriend(delete_friend_input_model model);
-    public ArrayList<Integer> DeleteFriend_test(delete_friend_input_model model);
+    //public ArrayList<Integer> DeleteFriend_test(delete_friend_input_model model);
 }
