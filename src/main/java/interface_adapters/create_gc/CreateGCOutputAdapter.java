bf2da815a1b72216025a0e021b335bf9d7c63177
Brenden <1mcfarlaneb1@gmail.com>
2022-11-30 16:14:32 -0500

    commitinng so i can pull functional code from main

--- src/main/java/interface_adapters/create_gc/CreateGCOutputAdapter.java
+++ src/main/java/interface_adapters/create_gc/CreateGCOutputAdapter.java
@@ -6,10 +6,15 @@ import usecases.create_gc.CreateGCOutputData;
 import usecases.retrieve_friendslist.RetrieveFriendsListOutputBoundary;
 import usecases.retrieve_friendslist.RetrieveFriendsListOutputData;
 
+
 public class CreateGCOutputAdapter implements CreateGCOutputBoundary {
     private IfComManager comManager;
     public CreateGCOutputAdapter(IfComManager comManager){ this.comManager = comManager;}
-
+    /**
+     * parses the CreateGCOutputData for a IfComManager.
+     *
+     * String has the format Constants.GROUP_CHAT
+     */
     @Override
     public void prepareSuccessView(CreateGCOutputData output_data) {
 
