b8d8f089c1461bedfcc303b3539ce15fbd899fa0
thmcheung <thmarcus.cheung@mail.utoronto.ca>
2022-12-05 21:26:26 -0500

    Register Interactor Test Completed

---
+++ src/test/java/test_usecases/test_register/DummyDB.java
@@ -0,0 +1,14 @@
+package test_usecases.test_register;
+import server.usecases.register.RegisterDBGateWay;
+
+public class DummyDB implements RegisterDBGateWay{
+
+    @Override
+    public int getNewUid(String username, String password){
+        return 1;
+    }
+    @Override
+    public void registerUser(int uid, String username, String password, String ipAddress){
+
+    }
+}
