98c84e549c4a95a3db0610ac664a3f933bb64930
thmcheung <105768082+thmcheung@users.noreply.github.com>
2022-12-08 16:21:10 -0500

    DOCSTRING

--- src/main/java/server/interface_adapters/logout/LogoutController.java
+++ src/main/java/server/interface_adapters/logout/LogoutController.java
@@ -2,6 +2,12 @@ package server.interface_adapters.logout;
 import server.frameworks_and_drivers.communication_manager.IfComManager;
 import server.usecases.logout.*;
 
+/*
+Receives a string called message,
+which contains the string of the uid of the user that would like to log out
+which the class then calls the LogoutInputBoundary in use cases file to execute the task
+*/
+
 public class LogoutController {
     final LogoutInputBoundary inbound;
 
