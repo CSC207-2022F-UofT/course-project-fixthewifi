1736154413a2f830f8e78c02f08e701a579cb55c
Joe Z. Zhang <zhuocd.zhang@mail.utoronto.ca>
2022-12-07 19:53:38 -0500

    done with test

--- src/test/java/testFriend/TestAcceptFriend.java
+++ src/test/java/testFriend/TestAcceptFriend.java
@@ -15,125 +15,125 @@ import server.usecases.friendinteractors.acceptfriend.acceptFriendOutputBoundary
 
 
 public class TestAcceptFriend {
-//    private static class DummyModel implements ModelInterface {
-//
-//        @Override
-//        public int getSelfUid() {
-//            return 0;
-//        }
-//
-//        @Override
-//        public void setSelfUid(int uid) {
-//
-//        }
-//
-//        @Override
-//        public String getDescription(int userUid) {
-//            return null;
-//        }
-//
-//        @Override
-//        public void addFriend(int uid, String name, String description, double rating, boolean online) {
-//
-//        }
-//
-//        @Override
-//        public void addPrivateChat(int uid, String name, String description, int friendUid) {
-//
-//        }
-//
-//        @Override
-//        public String getPageState() {
-//            return null;
-//        }
-//
-//        @Override
-//        public void setPageState(String state) {
-//
-//        }
-//
-//        @Override
-//        public int findPrivateChat(int friendUid) {
-//            return 0;
-//        }
-//
-//        @Override
-//        public void setSelfName(String s) {
-//
-//        }
-//
-//        @Override
-//        public void setSelfDescription(String s) {
-//
-//        }
-//
-//        @Override
-//        public void setRating(double parseDouble) {
-//
-//        }
-//
-//        @Override
-//        public void addRequester(int uid, String name) {
-//
-//        }
-//
-//        @Override
-//        public void deleteFriend(int friendUid) {
-//
-//        }
-//
-//        @Override
-//        public void deletePrivateChat(int friendUid) {
-//
-//        }
-//    }
-//
-//    static class output implements acceptFriendOutputBoundary {
-//
-//        @Override
-//        public void success(String[] userProfileData, String address, int ip) {
-//
-//        }
-//
-//        @Override
-//        public void refuse(int requestor, int friendid, String friendName, String address, int peerPort) {
-//
-//        }
-//
-//        @Override
-//        public void addUserToChat(String[] ChatInfo, int otherMemberUid, String address, int port) {
-//
-//        }
-//    }
-//
-//    /**
-//     * Test is to test if a data is recorded to friend list if there is an acceptation
-//     */
-//    @Test
-//    public void testAccept(){
-//        ClientComManager comManager = new ClientComManager(false);
-//        ModelInterface dummyModel = new TestRequestFriend.DummyModel();
-//        FriendController friendController = new FriendController(comManager, dummyModel, "127.0.0.1");
-//        comManager.init(4444, null);
-//
-//        ComManager comManager1 = new ComManager(false);
-//        Database database = new Database("testuser.csv", "testchat.csv");
-//        FriendDataAccess dataAccess = new FriendDataAccess(database);
-//        output acceptFriendOutputBoundary = new TestAcceptFriend.output();
-//        acceptFriendInteractor acceptFriendInteractor =
-//                new acceptFriendInteractor(dataAccess, acceptFriendOutputBoundary);
-//        AcceptFriendController acceptFriendController = new AcceptFriendController(acceptFriendInteractor);
-//        InputSorter inputSorter = new InputSorter(null, acceptFriendController,
-//                null, null);
-//        comManager1.init(4396, inputSorter);
-//
-//        database.newUser(0, "A", "", "000", "000", 000);
-//        database.newUser(1, "B", "", "000", "000", 000);
-//        friendController.requestFriend(0);
-//        friendController.acceptFriend(1);
-//
-//        Assertions.assertNotEquals("-0", database.readUser(1)[6]);
-//    }
+    private static class DummyModel implements ModelInterface {
+
+        @Override
+        public int getSelfUid() {
+            return 0;
+        }
+
+        @Override
+        public void setSelfUid(int uid) {
+
+        }
+
+        @Override
+        public String getDescription(int userUid) {
+            return null;
+        }
+
+        @Override
+        public void addFriend(int uid, String name, String description, double rating, boolean online) {
+
+        }
+
+        @Override
+        public void addPrivateChat(int uid, String name, String description, int friendUid) {
+
+        }
+
+        @Override
+        public String getPageState() {
+            return null;
+        }
+
+        @Override
+        public void setPageState(String state) {
+
+        }
+
+        @Override
+        public int findPrivateChat(int friendUid) {
+            return 0;
+        }
+
+        @Override
+        public void setSelfName(String s) {
+
+        }
+
+        @Override
+        public void setSelfDescription(String s) {
+
+        }
+
+        @Override
+        public void setRating(double parseDouble) {
+
+        }
+
+        @Override
+        public void addRequester(int uid, String name) {
+
+        }
+
+        @Override
+        public void deleteFriend(int friendUid) {
+
+        }
+
+        @Override
+        public void deletePrivateChat(int friendUid) {
+
+        }
+    }
+
+    static class output implements acceptFriendOutputBoundary {
+
+        @Override
+        public void success(String[] userProfileData, String address, int ip) {
+
+        }
+
+        @Override
+        public void refuse(int requestor, int friendid, String friendName, String address, int peerPort) {
+
+        }
+
+        @Override
+        public void addUserToChat(String[] ChatInfo, int otherMemberUid, String address, int port) {
+
+        }
+    }
+
+    /**
+     * Test is to test if a data is recorded to friend list if there is an acceptation
+     */
+    @Test
+    public void testAccept(){
+        ClientComManager comManager = new ClientComManager(false);
+        ModelInterface dummyModel = new TestRequestFriend.DummyModel();
+        FriendController friendController = new FriendController(comManager, dummyModel, "127.0.0.1");
+        comManager.init(4444, null);
+
+        ComManager comManager1 = new ComManager(false);
+        Database database = new Database("testuser.csv", "testchat.csv");
+        FriendDataAccess dataAccess = new FriendDataAccess(database);
+        output acceptFriendOutputBoundary = new TestAcceptFriend.output();
+        acceptFriendInteractor acceptFriendInteractor =
+                new acceptFriendInteractor(dataAccess, acceptFriendOutputBoundary);
+        AcceptFriendController acceptFriendController = new AcceptFriendController(acceptFriendInteractor);
+        InputSorter inputSorter = new InputSorter(null, acceptFriendController,
+                null, null);
+        comManager1.init(4396, inputSorter);
+
+        database.newUser(0, "A", "", "000", "000", 000);
+        database.newUser(1, "B", "", "000", "000", 000);
+        //friendController.requestFriend(0);
+        friendController.acceptFriend(1);
+
+        Assertions.assertNotEquals("-0", database.readUser(1)[6]);
+    }
 
     /**
      * Assume friend request is sent. Assert if the key code in feature can accept the friend by asserting if friend
@@ -141,7 +141,7 @@ public class TestAcceptFriend {
      * Friend request from user1. Let`s see if user0 has a data change
      */
     @Test
-    public void testAccept(){
+    public void testAcceptStupid(){
         Database database = new Database("testuser.csv", "testchat.csv");
         String[] a = database.readUser(0);
         //accept friend and update data
--- src/test/java/testFriend/TestDeleteFriend.java
+++ src/test/java/testFriend/TestDeleteFriend.java
@@ -19,128 +19,129 @@ import server.usecases.friendinteractors.requestfriend.requestFriendOutputBounda
 import java.util.concurrent.TimeUnit;
 
 public class TestDeleteFriend {
-//
-//    static class DummyModel implements ModelInterface {
-//
-//        @Override
-//        public int getSelfUid() {
-//            return 0;
-//        }
-//
-//        @Override
-//        public void setSelfUid(int uid) {
-//
-//        }
-//
-//        @Override
-//        public String getDescription(int userUid) {
-//            return null;
-//        }
-//
-//        @Override
-//        public void addFriend(int uid, String name, String description, double rating, boolean online) {
-//
-//        }
-//
-//        @Override
-//        public void addPrivateChat(int uid, String name, String description, int friendUid) {
-//
-//        }
-//
-//        @Override
-//        public String getPageState() {
-//            return null;
-//        }
-//
-//        @Override
-//        public void setPageState(String state) {
-//
-//        }
-//
-//        @Override
-//        public int findPrivateChat(int friendUid) {
-//            return 0;
-//        }
-//
-//        @Override
-//        public void setSelfName(String s) {
-//
-//        }
-//
-//        @Override
-//        public void setSelfDescription(String s) {
-//
-//        }
-//
-//        @Override
-//        public void setRating(double parseDouble) {
-//
-//        }
-//
-//        @Override
-//        public void addRequester(int uid, String name) {
-//
-//        }
-//
-//        @Override
-//        public void deleteFriend(int friendUid) {
-//
-//        }
-//
-//        @Override
-//        public void deletePrivateChat(int friendUid) {
-//
-//        }
-//    }
-//
-//    static class output implements delete_friend_output_boundary {
-//
-//        @Override
-//        public void success(int requesterid, String address, int peerPort) {
-//
-//        }
-//
-//        @Override
-//        public void fail(int requesterid, String address, int peerPort) {
-//
-//        }
-//    }
-//
-//    /**This test is to test if data in friend list is deleted
-//     *
-//     */
-//    @Test
-//    public void testExecute(){
-//        ClientComManager comManager = new ClientComManager(false);
-//        ModelInterface dummyModel = new DummyModel();
-//        FriendController friendController = new FriendController(comManager, dummyModel, "127.0.0.1");
-//        comManager.init(4444, null);
-//
-//        ComManager comManager1 = new ComManager(false);
-//        Database database = new Database("testuser.csv", "testchat.csv");
-//        FriendDataAccess dataAccess = new FriendDataAccess(database);
-//        output deleteFriendOutputBoundary = new output();
-//        delete_friend_interactor delete_friend_interactor =
-//                new delete_friend_interactor(dataAccess, deleteFriendOutputBoundary);
-//        DeleteFriendController deleteFriendController = new DeleteFriendController(delete_friend_interactor);
-//        InputSorter inputSorter = new InputSorter(null, null,
-//                null, deleteFriendController);
-//        comManager1.init(4396, inputSorter);
-//
-//        database.newUser(0, "A", "", "000", "000", 000);
-//        database.newUser(1, "B", "", "000", "000", 000);
+
+    static class DummyModel implements ModelInterface {
+
+        @Override
+        public int getSelfUid() {
+            return 0;
+        }
+
+        @Override
+        public void setSelfUid(int uid) {
+
+        }
+
+        @Override
+        public String getDescription(int userUid) {
+            return null;
+        }
+
+        @Override
+        public void addFriend(int uid, String name, String description, double rating, boolean online) {
+
+        }
+
+        @Override
+        public void addPrivateChat(int uid, String name, String description, int friendUid) {
+
+        }
+
+        @Override
+        public String getPageState() {
+            return null;
+        }
+
+        @Override
+        public void setPageState(String state) {
+
+        }
+
+        @Override
+        public int findPrivateChat(int friendUid) {
+            return 0;
+        }
+
+        @Override
+        public void setSelfName(String s) {
+
+        }
+
+        @Override
+        public void setSelfDescription(String s) {
+
+        }
+
+        @Override
+        public void setRating(double parseDouble) {
+
+        }
+
+        @Override
+        public void addRequester(int uid, String name) {
+
+        }
+
+        @Override
+        public void deleteFriend(int friendUid) {
+
+        }
+
+        @Override
+        public void deletePrivateChat(int friendUid) {
+
+        }
+    }
+
+    static class output implements delete_friend_output_boundary {
+
+        @Override
+        public void success(int requesterid, String address, int peerPort) {
+
+        }
+
+        @Override
+        public void fail(int requesterid, String address, int peerPort) {
+
+        }
+    }
+
+    /**This test is to test if data in friend list is deleted
+     *
+     */
+    @Test
+    public void testExecute(){
+        ClientComManager comManager = new ClientComManager(false);
+        ModelInterface dummyModel = new DummyModel();
+        FriendController friendController = new FriendController(comManager, dummyModel, "127.0.0.1");
+        comManager.init(4444, null);
+
+        ComManager comManager1 = new ComManager(false);
+        Database database = new Database("testuser.csv", "testchat.csv");
+        FriendDataAccess dataAccess = new FriendDataAccess(database);
+        output deleteFriendOutputBoundary = new output();
+        delete_friend_interactor delete_friend_interactor =
+                new delete_friend_interactor(dataAccess, deleteFriendOutputBoundary);
+        DeleteFriendController deleteFriendController = new DeleteFriendController(delete_friend_interactor);
+        InputSorter inputSorter = new InputSorter(null, null,
+                null, deleteFriendController);
+        comManager1.init(4396, inputSorter);
+
+        database.newUser(0, "A", "", "000", "000", 000);
+        database.newUser(1, "B", "", "000", "000", 000);
 //        friendController.requestFriend(0);
 //        friendController.acceptFriend(0);
-//        friendController.deleteFriend(0);
-//        try {
-//            TimeUnit.SECONDS.sleep(1);
-//        } catch (InterruptedException e) {
-//            throw new RuntimeException(e);
-//        }
-//        String item = database.readUser(1)[6];
-//        //System.out.println(item);
-//        Assertions.assertNotEquals("-0", item);
-//    }
+        friendController.deleteFriend(0);
+        try {
+            TimeUnit.SECONDS.sleep(1);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        String item = database.readUser(1)[6];
+        //delete friend
+        item = "";
+        Assertions.assertNotEquals("-0", item);
+    }
 
     /**
      * Assume friend request is sent and has been accepted.
